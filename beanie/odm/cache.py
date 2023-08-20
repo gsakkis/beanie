222db36b9f85818079aeef6a322d5c17a3423de2
George Sakkis <george.sakkis@gmail.com>
2023-08-20 17:29:53 +0300

    LRUCache: replace CachedItem model with tuple

--- beanie/odm/cache.py
+++ beanie/odm/cache.py
@@ -1,44 +1,30 @@
-import collections
-import datetime
-from datetime import timedelta
-from typing import Any, Optional
-
-from pydantic import BaseModel, Field
-
-
-class CachedItem(BaseModel):
-    timestamp: datetime.datetime = Field(
-        default_factory=datetime.datetime.utcnow
-    )
-    value: Any = None
+from collections import OrderedDict
+from datetime import datetime, timedelta
 
 
 class LRUCache:
     def __init__(self, capacity: int, expiration_time: timedelta):
         self.capacity: int = capacity
         self.expiration_time: timedelta = expiration_time
-        self.cache: collections.OrderedDict = collections.OrderedDict()
+        self.cache: OrderedDict = OrderedDict()
 
-    def get(self, key) -> Optional[CachedItem]:
+    def get(self, key):
         try:
             item = self.cache.pop(key)
-            if (
-                datetime.datetime.utcnow() - item.timestamp
-                > self.expiration_time
-            ):
+            if datetime.utcnow() - item[1] > self.expiration_time:
                 return None
             self.cache[key] = item
-            return item.value
+            return item[0]
         except KeyError:
             return None
 
-    def set(self, key, value) -> None:
+    def set(self, key, value):
         try:
             self.cache.pop(key)
         except KeyError:
             if len(self.cache) >= self.capacity:
                 self.cache.popitem(last=False)
-        self.cache[key] = CachedItem(value=value)
+        self.cache[key] = (value, datetime.utcnow())
 
     @staticmethod
     def create_key(*args):
