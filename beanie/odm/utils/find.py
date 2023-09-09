9a7a2084e59c9e5713bb1894b41c8c592d4d5e7b
George Sakkis <george.sakkis@gmail.com>
2023-09-09 13:12:41 +0300

    Merge _concise_lookup & _verbose_lookup into iter_stages

--- beanie/odm/utils/find.py
+++ beanie/odm/utils/find.py
@@ -5,20 +5,46 @@ from beanie.odm.links import LinkInfo
 
 
 def iter_stages(link_info: LinkInfo) -> Iterator[Dict[str, Any]]:
+    as_field = link_info.field_name
+    is_direct = "DIRECT" in link_info.link_type
+    if is_direct:
+        as_field = "_link_" + as_field
+
+    lookup: Dict[str, Any] = {
+        "from": link_info.document_class.get_collection_name(),
+        "as": as_field,
+    }
+
+    lookup_field = f"{link_info.lookup_field_name}.$id"
+    is_backlink = "BACK" in link_info.link_type
     if beanie.DATABASE_MAJOR_VERSION >= 5 or link_info.nested_links is None:
-        lookup = _concise_lookup(link_info)
+        local_field, foreign_field = lookup_field, "_id"
+        if is_backlink:
+            local_field, foreign_field = foreign_field, local_field
+        lookup["localField"] = local_field
+        lookup["foreignField"] = foreign_field
     else:
-        lookup = _verbose_lookup(link_info)
+        match_op = "$eq" if is_direct else "$in"
+        if is_backlink:
+            let_expr = {"link_id": "$_id"}
+            match_expr = ["$$link_id", "$" + lookup_field]
+        else:
+            let_expr = {"link_id": "$" + lookup_field}
+            match_expr = ["$_id", "$$link_id"]
+        lookup["let"] = let_expr
+        lookup["pipeline"] = [{"$match": {"$expr": {match_op: match_expr}}}]
+
     if link_info.nested_links is not None:
         pipeline = lookup.setdefault("pipeline", [])
         for nested_link_info in link_info.nested_links.values():
             pipeline.extend(iter_stages(nested_link_info))
+
     yield {"$lookup": lookup}
 
-    if "DIRECT" in link_info.link_type:
+    if is_direct:
         yield {
             "$unwind": {
-                "path": f"$_link_{link_info.field_name}",
+                "path": "$" + as_field,
                 "preserveNullAndEmptyArrays": True,
             }
         }
@@ -26,62 +52,11 @@ def iter_stages(link_info: LinkInfo) -> Iterator[Dict[str, Any]]:
             "$set": {
                 link_info.field_name: {
                     "$cond": {
-                        "if": {
-                            "$ifNull": [
-                                f"$_link_{link_info.field_name}",
-                                False,
-                            ]
-                        },
-                        "then": f"$_link_{link_info.field_name}",
-                        "else": f"${link_info.field_name}",
+                        "if": {"$ifNull": ["$" + as_field, False]},
+                        "then": "$" + as_field,
+                        "else": "$" + link_info.field_name,
                     }
                 }
             }
         }
-        yield {"$unset": f"_link_{link_info.field_name}"}
-
-
-def _concise_lookup(link_info: LinkInfo) -> Dict[str, Any]:
-    lookup_field = f"{link_info.lookup_field_name}.$id"
-    id_field = "_id"
-    if "BACK" in link_info.link_type:
-        local_field, foreign_field = id_field, lookup_field
-    else:
-        local_field, foreign_field = lookup_field, id_field
-
-    if "DIRECT" in link_info.link_type:
-        as_field = f"_link_{link_info.field_name}"
-    else:
-        as_field = link_info.field_name
-
-    return {
-        "from": link_info.document_class.get_collection_name(),
-        "localField": local_field,
-        "foreignField": foreign_field,
-        "as": as_field,
-    }
-
-
-def _verbose_lookup(link_info: LinkInfo) -> Dict[str, Any]:
-    lookup_field = f"${link_info.lookup_field_name}.$id"
-    id_field = "$_id"
-    if "BACK" in link_info.link_type:
-        link_id = id_field
-        expr = ["$$link_id", lookup_field]
-    else:
-        link_id = lookup_field
-        expr = [id_field, "$$link_id"]
-
-    if "DIRECT" in link_info.link_type:
-        match_expr = "$eq"
-        as_field = f"_link_{link_info.field_name}"
-    else:
-        match_expr = "$in"
-        as_field = link_info.field_name
-
-    return {
-        "from": link_info.document_class.get_collection_name(),
-        "let": {"link_id": link_id},
-        "pipeline": [{"$match": {"$expr": {match_expr: expr}}}],
-        "as": as_field,
-    }
+        yield {"$unset": as_field}
