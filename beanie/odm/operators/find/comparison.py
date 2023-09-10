a184c74de40d6152f20ba14d5af457ada35bbea9
George Sakkis <george.sakkis@gmail.com>
2023-09-11 02:23:58 +0300

    Refactor comparison operators

--- beanie/odm/operators/find/comparison.py
+++ beanie/odm/operators/find/comparison.py
@@ -4,11 +4,7 @@ from beanie.odm.operators import BaseOperator
 class BaseFindComparisonOperator(BaseOperator):
     operator = ""
 
-    def __init__(
-        self,
-        field,
-        other,
-    ):
+    def __init__(self, field, other):
         self.field = field
         self.other = other
 
@@ -19,22 +15,7 @@ class BaseFindComparisonOperator(BaseOperator):
 
 class Eq(BaseFindComparisonOperator):
     """
-    `equal` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    Eq(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": 2}
-    ```
+    `$eq` query operator
 
     MongoDB doc:
     <https://docs.mongodb.com/manual/reference/operator/query/eq/>
@@ -45,24 +26,20 @@ class Eq(BaseFindComparisonOperator):
         return {self.field: self.other}
 
 
+class NE(BaseFindComparisonOperator):
+    """
+    `$ne` query operator
+
+    MongoDB doc:
+    <https://docs.mongodb.com/manual/reference/operator/query/ne/>
+    """
+
+    operator = "$ne"
+
+
 class GT(BaseFindComparisonOperator):
     """
     `$gt` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    GT(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$gt": 2}}
-    ```
 
     MongoDB doc:
     <https://docs.mongodb.com/manual/reference/operator/query/gt/>
@@ -75,21 +52,6 @@ class GTE(BaseFindComparisonOperator):
     """
     `$gte` query operator
 
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    GTE(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$gte": 2}}
-    ```
-
     MongoDB doc:
     <https://docs.mongodb.com/manual/reference/operator/query/gte/>
     """
@@ -97,76 +59,9 @@ class GTE(BaseFindComparisonOperator):
     operator = "$gte"
 
 
-class In(BaseFindComparisonOperator):
-    """
-    `$in` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    In(Product.price, [2, 3, 4])
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$in": [2, 3, 4]}}
-    ```
-
-    MongoDB doc:
-    <https://docs.mongodb.com/manual/reference/operator/query/in/>
-    """
-
-    operator = "$in"
-
-
-class NotIn(BaseFindComparisonOperator):
-    """
-    `$nin` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    NotIn(Product.price, [2, 3, 4])
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$nin": [2, 3, 4]}}
-    ```
-
-    MongoDB doc:
-    <https://docs.mongodb.com/manual/reference/operator/query/nin/>
-    """
-
-    operator = "$nin"
-
-
 class LT(BaseFindComparisonOperator):
     """
     `$lt` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    LT(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$lt": 2}}
-    ```
 
     MongoDB doc:
     <https://docs.mongodb.com/manual/reference/operator/query/lt/>
@@ -179,21 +74,6 @@ class LTE(BaseFindComparisonOperator):
     """
     `$lte` query operator
 
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    LTE(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$lte": 2}}
-    ```
-
     MongoDB doc:
     <https://docs.mongodb.com/manual/reference/operator/query/lte/>
     """
@@ -201,27 +81,23 @@ class LTE(BaseFindComparisonOperator):
     operator = "$lte"
 
 
-class NE(BaseFindComparisonOperator):
+class In(BaseFindComparisonOperator):
     """
-    `$ne` query operator
-
-    Example:
-
-    ```python
-    class Product(Document):
-        price: float
-
-    NE(Product.price, 2)
-    ```
-
-    Will return query object like
-
-    ```python
-    {"price": {"$ne": 2}}
-    ```
+    `$in` query operator
 
     MongoDB doc:
-    <https://docs.mongodb.com/manual/reference/operator/query/ne/>
+    <https://docs.mongodb.com/manual/reference/operator/query/in/>
     """
 
-    operator = "$ne"
+    operator = "$in"
+
+
+class NotIn(BaseFindComparisonOperator):
+    """
+    `$nin` query operator
+
+    MongoDB doc:
+    <https://docs.mongodb.com/manual/reference/operator/query/nin/>
+    """
+
+    operator = "$nin"
